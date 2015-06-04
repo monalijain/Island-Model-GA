88a46e3481a4664000868cacec1ad841a00b4fc7
monalijain <monalijain419@gmail.com>
2015-06-04 20:42:23 +0530

    Update IslandParameters.py

--- IslandParameters.py
+++ IslandParameters.py
@@ -1,7 +1,7 @@
 __author__ = 'Monali'
 
 
-NumSubpop=10 #Number of subpopulations, i.e. number of islands in the model
+NumSubpop=5 #Number of subpopulations, i.e. number of islands in the model
 #SubPopSize=MaxIndividuals/NumSubpop
 LengthEpoch=10 #Number of Generations for which no migration (islands run independently)
 MigrantSize=0.25 #defined as a percentage of a generation's population of an island which migrates to another island
